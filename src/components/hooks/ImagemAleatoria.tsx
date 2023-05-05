d6b354835de00f65e6d81ecd272a27634b35eb61
Farney <f_farney@hotmail.com>
2023-05-05 17:01:10 -0300

    Estado e comunicação

--- src/components/hooks/ImagemAleatoria.tsx
+++ src/components/hooks/ImagemAleatoria.tsx
@@ -1,7 +1,27 @@
 export default function ImagemAleatoria() {
+
+    const url = 'https://source.unsplash.com/featured/300x300'
+
+    let pesquisa: string = ''
+
+    function renderizarBotao(valor: string) {
+        return (
+            <button className={`
+
+                bg-blue-500 px-4 py-2 rounded-md            
+            
+            `} onClick={() => pesquisa = valor}>
+                {valor}
+            </button>
+        )
+    }
+
+
     return (
-        <div>
-            ImagemAleatoria
+        <div className=" flex flex-col gap-3">
+            {renderizarBotao('abstract')}
+            {renderizarBotao('city')}
+            {renderizarBotao('person')}
         </div>
     )
 }
